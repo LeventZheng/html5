1ee0f5677de7182b3825ff876892686a3ad3edf9
levent_zheng <bszhenglf@163.com>
2016-01-11 22:17:58 +0800

    增加angularjs无限树形下拉框

---
+++ angular/无限级树形下拉搜索框/v1.2/readme.js
@@ -0,0 +1,7 @@
+tree-view v1.2 
+tree-data="data"  //调用的data   @list
+return-data="returnData"  //点击返回的值 @string
+数据结构为
+data=[{text:"name",id:"1",children:[]}]
+文件夹只是装饰，无展开功能,以后再扩展吧，
+如有问题，可mail29124088@qq.com
