6e91ef488ee12ea4b6768bb38bdc919b76f6e4e7
LeventZheng <bszhenglf@163.com>
2016-06-16 09:24:50 +0800

    angular模块更新

--- javascript/算法/二分插入排序.js
+++ javascript/算法/二分插入排序.js
@@ -3,14 +3,28 @@
  */
 function devideSort(array){
     if(Object.prototype.toString.call(array).slice(8,-1) === 'Array'){
-        var newArr = [array[0]];
         for(var i=1; i<array.length; i++){
-
+            var key = array[i];
+            var  left = 0, right = i-1;
+            while(left <= right){
+            	var middle = parseInt((left + right)/2);
+                console.log('middle:'+array[middle]+",key:"+key);
+                if(array[middle]>key){
+            		right = middle - 1;
+            	}else{
+            		left = middle + 1;
+            	}
+                console.log('left:'+left+",right:"+right);
+            }
+            for(var j = i; j>left; j--){
+                array[j] = array[j-1];
+            }
+            array[left] = key;
         }
-        return true;
+        console.log(array);
     }else{
         return 'this is not a Array.'
     }
 }
-
-console.log(devideSort([1,2,3]));
+devideSort([1,8,2,7,5,9,3]);
+//console.log(devideSort([1,3,8,4,9,6,5,4]));
