30c5e3818984af6ad78f838d3be9011860f02009
bszhenglf <742535321@qq.com>
2019-01-23 16:37:02 +0800

    归并排序

--- 算法与数据结构/sort/array/merge/sort.js
+++ 算法与数据结构/sort/array/merge/sort.js
@@ -5,7 +5,7 @@
  */
 function MergeSort(arr, selector) {
     selector = selector || JSON.parse;
-    return _MergeSort(arr, 0, arr.length);
+    return _MergeSort(arr, 0, arr.length-1);
 }
 /**
  * 
@@ -14,22 +14,48 @@ function MergeSort(arr, selector) {
  * @param {*} r 右边界
  */
 function _MergeSort(arr, l, r) {
-    let middle = Math.ceil((l + r) / 2);
-    // 边界即退出条件
-    if (middle == 0) return arr;
-
-    // 切分的范围是 [l, middle] 和 (middle, r];
-    let arrLeft = _MergeSort(arr.slice(l, middle), l, middle);  // 获取左边归并排序后的数组
-    let arrRight = _MergeSort(arr.slice(middle+1), middle, r); // 获取右边归并排序后的数组
+    if (l < r) {
+        let middle = Math.floor((l + r) / 2);
+        // 切分的范围是 [l, middle] 和 (middle, r];
+        _MergeSort(arr, l, middle);  // 获取左边归并排序后的数组
+        _MergeSort(arr, middle+1, r); // 获取右边归并排序后的数组
+        _Merge(arr, l, middle, r)
+    }
+}
+function _Merge(arr, l, middle, r) {
     // 合并左右数组
     let mergeArr = new Array(); // 数组长度是 l + r -1
     let k = 0;  // 当前存放对比值存放在 mergeArr 中的位置
     let i = l, j = middle + 1;
-    while(k < l + r -1) {
-        if (arrLeft[i] < arrRight[j] && i <= middle) {
-            mergeArr[k] = arrLeft[i];
+
+    // 遍历正常的情况
+    while (i <= middle && j <= r) {
+        if (arr[i] > arr[j]) {
+            mergeArr[k] = arr[j];
+            k++;
+            j++;
+        } else {
+            mergeArr[k] = arr[i];
             k++;
             i++;
         }
+    }
+    // i 还没到终点，把左边剩余元素顺序移入 mergeArr
+    while (i <= middle) {
+        mergeArr[k] = arr[i];
+        k++;
+        i++;
+    }
+    // j 还没到重点，把有点剩余元素顺序移入 mergeArr
+    while (j <= r) {
+        mergeArr[k] = arr[j];
+        k++;
+        j++;
+    }
+
+    t = 0;
+    //将 mergeArr 中的元素全部拷贝到原数组中
+    while(l <= r){
+        arr[l++] = mergeArr[t++];
     }
 }
