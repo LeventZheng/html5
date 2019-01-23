cce5fd4b00ef0d870115bc4e5831ef098b0447cc
bszhenglf <742535321@qq.com>
2019-01-23 16:59:54 +0800

    归并排序及其优化

--- 算法与数据结构/sort/array/merge/sort.js
+++ 算法与数据结构/sort/array/merge/sort.js
@@ -19,7 +19,12 @@ function _MergeSort(arr, l, r) {
         // 切分的范围是 [l, middle] 和 (middle, r];
         _MergeSort(arr, l, middle);  // 获取左边归并排序后的数组
         _MergeSort(arr, middle+1, r); // 获取右边归并排序后的数组
-        _Merge(arr, l, middle, r)
+
+        // 判断是否需要归并
+        // 当左边的最后一个元素比右边的第一个小，说明左边部分全部比右边小，两边都已经排好序了
+        if (arr[middle] > arr[middle+1]) {
+            _Merge(arr, l, middle, r)
+        }
     }
 }
 function _Merge(arr, l, middle, r) {
@@ -57,5 +62,44 @@ function _Merge(arr, l, middle, r) {
     //将 mergeArr 中的元素全部拷贝到原数组中
     while(l <= r){
         arr[l++] = mergeArr[t++];
+    }
+}
+
+/**
+ * 归并排序优化版-当排序区间小于8，改用插入排序
+ * @param {*} arr 
+ * @param {*} selector 
+ */
+function MergeSortOptimize(arr, selector) {
+    selector = selector || JSON.parse;
+    return _MergeSort(arr, 0, arr.length-1);
+}
+function _MergeSortOptimize(arr, l, r) {
+    if (r - l < 8) {
+        _InsertionSort(arr, l, r);
+        return;
+    }
+    let middle = Math.floor((l + r) / 2);
+    // 切分的范围是 [l, middle] 和 (middle, r];
+    _MergeSortOptimize(arr, l, middle);  // 获取左边归并排序后的数组
+    _MergeSortOptimize(arr, middle+1, r); // 获取右边归并排序后的数组
+
+    // 判断是否需要归并
+    // 当左边的最后一个元素比右边的第一个小，说明左边部分全部比右边小，两边都已经排好序了
+    if (arr[middle] > arr[middle+1]) {
+        _Merge(arr, l, middle, r)
+    }
+}
+function _InsertionSort(arr, l, r) {
+    for (let i=l; i<=r; i++) {
+        let target = arr[i];
+        for (j=i-1; j>=0; j--) {
+            if (arr[j]>arr[j+1]) {
+                arr[j+1] = arr[j];
+            } else {
+                arr[j+1] = target;
+                break;
+            }
+        }
     }
 }
